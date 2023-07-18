1c64db4de0755a7f071f3e86ffff5fab37595364
Ydahyun <y7d3h0@naver.com>
2023-07-19 00:41:40 +0900

    Update 03-1_FeatureEngineering.py

--- Chap03/03-1_FeatureEngineering.py
+++ Chap03/03-1_FeatureEngineering.py
@@ -1,10 +1,18 @@
 # 여러 개의 특성을 사용한 선형 회귀를 다중회귀(multiple regresson)라고 함.
 # 특성 공학은 주어진 특성을 조합하여 새로운 특성을 만드는 일련의 작업 과정이다.
-
+# 규제(regularization)는 머신러닝 모델이 훈련 세트를 너무 과도하게 학습하지 못하도록 훼방하는 것
+# 즉 모델이 훈련세트에 과대적합되지 않도록 만드는 것.
+# 선형 회귀 모델의 경우 특성에 곱해지는 계수(또는 기울기)의 크기를 작게 만드는 것임.
+# 릿지(계수를 제곱한 값을 기준으로 규제 적용) 와 라쏘(계수의 절댓값을 기준으로 규제를 적용)가 있다.
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Ridge
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Lasso
 
 df = pd.read_csv('https://bit.ly/perch_csv_data')
 perch_full = df.to_numpy()
@@ -20,6 +28,83 @@ perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 8
 
 train_input, test_input, train_target, test_target = train_test_split(perch_full, perch_weight, random_state=42)
 
-poly = PolynomialFeatures()
-poly.fit([[2,3]])
-print(poly.transform([[2,3]]))
+poly = PolynomialFeatures(include_bias=False)
+poly.fit(train_input)
+train_poly = poly.transform(train_input)
+print(train_poly.shape)
+
+test_poly = poly.transform(test_input)
+
+lr = LinearRegression()
+lr.fit(train_poly, train_target)
+print(lr.score(train_poly, train_target))
+print(lr.score(test_poly, test_target))
+
+
+
+ss = StandardScaler()
+ss.fit(train_poly)
+train_scaled = ss.transform(train_poly)
+test_scaled = ss.transform(test_poly)
+
+# 릿지
+ridge = Ridge()
+ridge.fit(train_scaled, train_target)
+print(ridge.score(train_scaled, train_target))
+
+train_score = []
+test_score = []
+alpha_list = [0.001, 0.01, 0.1, 1, 10, 100]
+for alpha in alpha_list:
+    # 릿지 모델을 만듭니다
+    ridge = Ridge(alpha=alpha)
+    # 릿지 모델을 훈련합니다
+    ridge.fit(train_scaled, train_target)
+    # 훈련 점수와 테스트 점수를 저장합니다
+    train_score.append(ridge.score(train_scaled, train_target))
+    test_score.append(ridge.score(test_scaled, test_target))
+
+plt.plot(np.log10(alpha_list), train_score)
+plt.plot(np.log10(alpha_list), test_score)
+plt.xlabel('alpha')
+plt.ylabel('R^2')
+plt.show()
+
+ridge = Ridge(alpha=0.1)
+ridge.fit(train_scaled, train_target)
+
+print(ridge.score(train_scaled, train_target))
+print(ridge.score(test_scaled, test_target))
+
+# 라쏘
+lasso = Lasso()
+lasso.fit(train_scaled, train_target)
+print(lasso.score(train_scaled, train_target))
+print(lasso.score(test_scaled, test_target))
+
+train_score = []
+test_score = []
+
+alpha_list = [0.001, 0.01, 0.1, 1, 10, 100]
+for alpha in alpha_list:
+    # 라쏘 모델을 만듭니다
+    lasso = Lasso(alpha=alpha, max_iter=10000)
+    # 라쏘 모델을 훈련합니다
+    lasso.fit(train_scaled, train_target)
+    # 훈련 점수와 테스트 점수를 저장합니다
+    train_score.append(lasso.score(train_scaled, train_target))
+    test_score.append(lasso.score(test_scaled, test_target))
+
+plt.plot(np.log10(alpha_list), train_score)
+plt.plot(np.log10(alpha_list), test_score)
+plt.xlabel('alpha')
+plt.ylabel('R^2')
+plt.show()
+
+lasso = Lasso(alpha=10)
+lasso.fit(train_scaled, train_target)
+
+print(lasso.score(train_scaled, train_target))
+print(lasso.score(test_scaled, test_target))
+
+print(np.sum(lasso.coef_ == 0))
