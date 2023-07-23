f265dccc2e822327be023928e6885d1691930c4d
Ydahyun <y7d3h0@naver.com>
2023-07-23 19:29:51 +0900

    Update 01_LogisticRegressoion.py

--- Chap04/01_LogisticRegressoion.py
+++ Chap04/01_LogisticRegressoion.py
@@ -3,6 +3,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from scipy.special import expit
+
+# 로지스틱 회귀 학습 전 k-최근접 이웃 분류기의 확률 예측
 fish = pd.read_csv('https://bit.ly/fish_csv_data')
 fish.head()
 
@@ -37,3 +42,36 @@ proba = kn.predict_proba(test_scaled[:5])
 print()
 print("'Bream' 'Parkki' 'Perch' 'Pike' 'Roach' 'Smelt' 'Whitefish'")
 print(np.round(proba, decimals=4))  # 각 클래스의 확률
+
+# 로지스틱 회귀는 이름은 회귀이지만 분류 모델이다. 이 알고리즘은 선형 회귀와 동일하게 선형 방정식을 학습한다.
+# ex. z = a*(Weight) + b*(Length) + c*(Diagonal) + d*(Height) + e*(Width) + f
+# a,b,c,d,e는 가중치 혹은 계수이며 z는 어떤 값도 가능하다. 확률로 표현하기위해(0과 1사이) 시그모이드함수를 사용.
+z = np.arange(-5,5,0.1)
+phi = 1 / (1 + np.exp(-z))
+plt.plot(z, phi)
+plt.xlabel('z')
+plt.ylabel('phi')
+plt.show()
+
+# 로지스틱 회귀로 이진 분류 수행하기
+
+# 넘파이 배열은 불리언 인덱싱이라고 True만 전달할 수 있다.
+char_arr = np.array(['A','B','C','D','E'])
+print(char_arr[[True, False, True, False, False]])
+# 이와같이 도미(Bream)과 빙어(Smelt)의 행만 골라내면
+bream_smelt_indexes = (train_target =='Bream') | (train_target == 'Smelt')
+train_bream_smelt = train_scaled[bream_smelt_indexes]
+target_bream_smelt = train_target[bream_smelt_indexes]
+
+lr = LogisticRegression()
+lr.fit(train_bream_smelt, target_bream_smelt)
+
+print(lr.predict(train_bream_smelt[:5]))
+print(lr.predict_proba(train_bream_smelt[:5]))
+print(lr.classes_)
+print(lr.coef_, lr.intercept_)
+
+# z값 계산
+decisions = lr.decision_function(train_bream_smelt[:5])
+print(decisions)  # 이 z값을 시그모이드 함수에 통과시키면 확률을 얻을 수 있다. expit(), np.exp()
+print(expit(decisions))
